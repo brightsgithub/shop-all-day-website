cfff1dc0c8561e1c44ccded7078671003a8b9c6a
BrightPersonal <brightpersonal@Brights-Mac-mini.local>
2024-06-29 13:23:25 +0100

    using dynamic host name in app constants

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,6 +1,4 @@
-const hostName = "ec2-13-40-6-197.eu-west-2.compute.amazonaws.com"; // Constant declaration for hostname
+const hostName = window.location.hostname; // Dynamically get the host name from the current environment
 const serverUrl = `http://${hostName}:8080`; // Template literal for URL construction
 
 export { serverUrl };
-
-
